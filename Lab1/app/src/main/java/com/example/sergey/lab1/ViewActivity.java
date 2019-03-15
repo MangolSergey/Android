98cab9c6478ca5bf3cbf3771c4fb397b4ba1ba12
Sergey Mangol <s.mangol@inbox.ru>
2019-03-15 14:11:08 +0300

    Lab1

---
+++ Lab1/app/src/main/java/com/example/sergey/lab1/ViewActivity.java
@@ -0,0 +1,22 @@
+package com.example.sergey.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ViewActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        textView = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        textView.setText(name);
+    }
+}
